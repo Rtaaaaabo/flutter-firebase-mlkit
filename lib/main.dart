37f037f90eee57f5c39af4912f34a76e5102c816
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-11-29 23:44:04 +0900

    DummyDataを代入

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,91 @@
 import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-import 'package:flutter_camera_ml_vision/flutter_camera_ml_vision.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+// import 'package:flutter_camera_ml_vision/flutter_camera_ml_vision.dart';
 
 void main() => runApp(MyApp());
+
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'ML Kit flutter'),
+      title: "Baby Name",
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> data = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RaisedButton(
-            child: Text('Scan product'),
-            onPressed: () async {
-              final barcord = await Navigator.of(context)
-                  .push<Barcode>(MaterialPageRoute(builder: (c) {
-                return ScanPage();
-              }));
-              if (barcord == null) {
-                return;
-              }
-
-              setState(() {
-                data.add(barcord.displayValue);
-              });
-            },
-          ),
-          Expanded(
-            child: ListView(
-              children: data.map((d) => Text(d)).toList(),
-            ),
-          )
-        ],
-      ),
-    );
+  _MyHomePageState createState() {
+    return _MyHomePageState();
   }
 }
 
-class ScanPage extends StatefulWidget {
-  @override
-  _ScanPageState createState() => _ScanPageState();
-}
-
-class _ScanPageState extends State<ScanPage> {
-  bool resultSent = false;
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: CameraMlVision<List<Barcode>>(
-            detector: FirebaseVision.instance.barcodeDetector().detectInImage,
-            onResult: (List<Barcode> barcodes) {
-              if (!mounted || resultSent) {
-                return;
-              }
-              resultSent = true;
-              Navigator.of(context).pop<Barcode>(barcodes.first);
-            },
-          ),
+      appBar: AppBar(title: Text('Body Name Votes')),
+      body: _builderBody(context),
+    );
+  }
+
+  Widget _builderBody(BuildContext context) {
+    return _builderList(context, dummySnapshot);
+  }
+
+  Widget _builderList(BuildContext context, List<Map> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children:
+          snapshot.map((data) => _builderListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _builderListItem(BuildContext context, Map data) {
+    final record = Record.fromMap(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => print(record),
         ),
       ),
     );
   }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
 }
